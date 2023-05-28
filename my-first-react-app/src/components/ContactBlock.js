62c3e15dae7f5284535d5f6866246cf4ddb17c1a
YuliiaIIIII <yulyamashka@gmail.com>
2023-05-28 22:17:08 +0200

    created new branch

--- my-first-react-app/src/components/ContactBlock.js
+++ my-first-react-app/src/components/ContactBlock.js
@@ -1,43 +1,42 @@
 import { Counter } from "./Counter";
 
 export function ContactBlock() {
-
-    return (
-      <div id="contacts">
-        <div className="container">
-          <div className="profile-name fw600">
-            Yuliia <span className="surname fw600">Malykhina</span>  
+  return (
+    <div id="contacts">
+      <div className="container">
+        <div className="profile-name fw600">
+          Yuliia <span className="surname fw600">Malykhina</span>  
+        </div>
+        <div>
+          <h3 className="">Contacts</h3>
+        </div>
+        <div className="allContacts">
+          <div className="contactInfo">
+            <h4>Address</h4> 
+            Kyiv, Ukraine 
           </div>
-          <div>
-            <h3 className="">Contacts</h3>
+          <div className="contactInfo">
+            <h4>Phone</h4>
+            +38 067 545 35 47 
           </div>
-          <div className="allContacts">
-            <div className="contactInfo">
-              <h4>Address</h4> 
-              Kyiv, Ukraine 
-            </div>
-            <div className="contactInfo">
-              <h4>Phone</h4>
-              +38 067 545 35 47 
-            </div>
-            <div className="contactInfo">
-              <h4>E-mail</h4> 
-              <div className="blueText">yulyamashka@gmail.com</div> 
-            </div>
-            <div className="contactInfo">
-              <h4>LinkedIn</h4> 
-              <a href="https://www.linkedin.com/in/yulia-malykhina-80b202166/">https://www.linkedin.com/in/Malykhina</a>
-            </div>
+          <div className="contactInfo">
+            <h4>E-mail</h4> 
+            <div className="blueText">yulyamashka@gmail.com</div> 
+          </div>
+          <div className="contactInfo">
+            <h4>LinkedIn</h4> 
+            <a href="https://www.linkedin.com/in/yulia-malykhina-80b202166/">https://www.linkedin.com/in/Malykhina</a>
           </div>
         </div>
-        <div className="counter fw600">
-          <div>
-            Send CV
-          </div>
-          <div>
-            <Counter />
-          </div>
+      </div>
+      <div className="counter fw600">
+        <div>
+          Send CV
         </div>
+        <div>
+          <Counter />
+        </div>
+      </div>
     </div>
   );
 }
