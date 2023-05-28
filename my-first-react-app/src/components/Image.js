46113f7d996913e6c0364d5b77e3eadedd205b57
YuliiaIIIII <yulyamashka@gmail.com>
2023-05-28 22:20:28 +0200

    created new branch

--- my-first-react-app/src/components/Image.js
+++ my-first-react-app/src/components/Image.js
@@ -1,7 +1,7 @@
 export function Image({userImage, imageAlt}) {
   return (
     <div id="selfie">
-        <img src={userImage} alt={imageAlt} />
+      <img src={userImage} alt={imageAlt} />
     </div>
   );
 }
